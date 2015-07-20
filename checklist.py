1fd0763d524a80a94f6059cab63b0b76f248ab88
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2015-07-20 18:23:03 -0500

    two more templates

--- checklist.py
+++ checklist.py
@@ -1,16 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import redirect
+from flask import url_for
+from hashids import Hashids
+from time import time
 
 app = Flask(__name__)
+hashids = Hashids(
+    salt = 'y05VG4El4$4QdkNB0{IWXkmp<|Ba+yo6',
+    min_length = 10,
+)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/check/<check>')
+@app.route('/c/<check>')
 def show_check(check):
     if check == 'new':
-        pass
+        curtime = time()
+        return redirect(url_for('show_check',
+            check=hashids.encode(int(curtime), int((curtime%1)*10000000))
+        ))
     return render_template('check.html')
 
 if __name__ == "__main__":
